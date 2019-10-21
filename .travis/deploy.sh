1fd9f1630403c9bcbdc1fac62d2c28115faabc4a
Alexander <Alexander_Sarson@outlook.com>
2019-10-21 14:49:15 +0200

    test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,12 +4,9 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/deploy_rsa # Allow read access to the private key
 ssh-add .travis/deploy_rsa # Add the private key to SSH
 
-git config --global push.default matching
-git remote add deploy ssh://alex@$IP:$PORT$DEPLOY_DIR
-git push deploy master
+scp -p target/*.war -P $PORT alex@$IP:/opt/tomcat/webapps/root.war
 
-# Skip this command if you don't need to execute any additional commands after deploying.
 ssh alex@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
-  cp -f root.war /opt/tomcat/webapps
+#  cp -f root.war /opt/tomcat/webapps
 EOF
