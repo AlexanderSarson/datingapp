208c55caa8c64da9ba5a4ed5ccf01ed26f8636b9
Alexander <Alexander_Sarson@outlook.com>
2019-10-21 14:52:19 +0200

    test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,7 +4,7 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/deploy_rsa # Allow read access to the private key
 ssh-add .travis/deploy_rsa # Add the private key to SSH
 
-scp -p target/*.war -P $PORT alex@$IP:/opt/tomcat/webapps/root.war
+scp -p target/*.war -P $PORT alex@$IP:/opt/tomcat/webapps/
 
 ssh alex@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
