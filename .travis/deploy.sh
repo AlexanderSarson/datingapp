1e6a4a92a48396cb36461fdabb4d61c9a4f0b100
Alexander <Alexander_Sarson@outlook.com>
2019-10-21 15:38:31 +0200

    test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,11 +5,11 @@ chmod 600 .travis/deploy_rsa # Allow read access to the private key
 ssh-add .travis/deploy_rsa # Add the private key to SSH
 
 echo scp
-scp -p target/\*.war $USER@$IP:/opt/tomcat/webapps/
+scp -p target/root.war $USERNAME@$IP:/opt/tomcat/webapps/
 #echo rsync
 #rsync -avz target/root.war alex@$IP:/opt/tomcat/webapps/
 
-ssh $USER@$IP -p $PORT <<EOF
+ssh $USERNAME@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
 #  cp -f root.war /opt/tomcat/webapps
 EOF
