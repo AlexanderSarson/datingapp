7c6316d7752098ccb423e98f61954a4a3dea0fbf
Alexander <Alexander_Sarson@outlook.com>
2019-10-21 15:42:29 +0200

    test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,12 +4,4 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/deploy_rsa # Allow read access to the private key
 ssh-add .travis/deploy_rsa # Add the private key to SSH
 
-echo scp
 scp -p target/root.war $USERNAME@$IP:/opt/tomcat/webapps/
-#echo rsync
-#rsync -avz target/root.war alex@$IP:/opt/tomcat/webapps/
-
-ssh $USERNAME@$IP -p $PORT <<EOF
-  cd $DEPLOY_DIR
-#  cp -f root.war /opt/tomcat/webapps
-EOF
